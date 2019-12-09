a0304c3d49848857b1dd8eace183decc16b42365
Lora Breskaja <lora@Loras-MacBook.local>
2019-12-09 21:12:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<header class="site-header" id="myHeader">
 			<div class="container flex-container">
 				<div class="logo flex-container">
-					<a href="#"> <img src="assets/images/logo.svg" alt="Weinberg"></a>
+					<a href="index.php"> <img src="assets/images/logo.svg" alt="Weinberg"></a>
 					<p>capital</p>
 				</div>
 				
@@ -24,20 +24,20 @@
 					</div>
 				 <ul class="flex-container mainMenu">
 					<li><a href="index.php">Home</a></li>
-					<li><a href="#">FAQ</a></li>
-					<li><a href="#">Apply</a></li>
+					<li><a id="scrollToFaq" href="#">FAQ</a></li>
+					<li><a id="scrollToApply" href="#">Apply</a></li>
 				</ul>
 				</nav> 
+			</div>
 		</header>
 
 		<section>
 			<div class="container flex-container hero">
 				<div class="hero-section">
 					<h1>Mortgage, leasing, and personal loans at your fingertips.</h1>
-
 					<div class="flex-container calc">
-						<button>Calculate</button>
-						<p>your monthly installment</p>
+						<button class="scrollToCalculator">Calculate</button>
+						<p>your monthly instalment</p>
 					</div>
 				</div>
 				<div class="hero-section">
@@ -68,7 +68,7 @@
 					<i class="fas fa-car"></i>
 					</div>
 				</div>
-				<div class="column">
+				<div id="scrollToFaq2" class="column">
 					<img src="assets/images/opti/bags.png" alt="Personal loans">
 					<h3>Personal loans</h3>
 					<div class="flex-container">
@@ -89,7 +89,7 @@
 					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
 					<div class="flex-container">
 						<i class="fas fa-question-circle"></i>
-						<a href="#" id="modalFaq" class="faqbutton">Frequently Asked Questions</a>
+						<a id="modalFaq" class="faqbutton scrollToCalculator2" href="#">Frequently Asked Questions</a>
 						<div id="simpleLink" class="link">
 							<div class="faq-content">
 								<span class="closeFaq">&times;</span>
@@ -125,10 +125,10 @@
 			</div>
 			<div class="container flex-container calculator">
 				<div class="calculator-one">
-					<h1>Calculate your monthly installment</h1>
+					<h1>Calculate your monthly instalment</h1>
 					<p>Use the mortgage loan calculator to calculate a loan instalment that you could pay the bank every month without difficulty and consider the risk of decreased income.</p>
-				</div class="calculator-two">
-					<div  class="tabContainer">
+				</div>
+					<div class="calculator-two tabContainer">
 						<div class="buttonContainer">
 							<button onclick="showPanel(0, '#ffffff')">Mortgage</button>
 							<button onclick="showPanel(1, '#ffffff')">Leasing</button>
@@ -153,7 +153,7 @@
 
 							<div class="flex-container">
 								<p>Interest rate</p>
-								<p class="readouts">2.0%</p>
+								<p class="readouts">3.0%</p>
 							</div>
 
 							<div class="flex-container">
@@ -165,27 +165,27 @@
 							<div class="slidecontainer">
 							<div class="flex-container">
 							<p>Amount</p>
-							<p class="readouts">$ <span id="loans"></span></p>
+							<p class="readouts">$ <span id="loans2"></span></p>
 							</div>
-  							<input type="range" min="1" max="25000" value="8000" class="slider" id="myLoanRange" onchange="computeLoan()">
+  							<input type="range" min="1" max="25000" value="8000" class="slider3" id="myLoanRange2" onchange="computeLoan2()">
 							</div>
 
 							<div class="slidecontainer">
 							<div class="flex-container">
 							<p>Period</p>
-							<p class="readouts"><span id="period"></span> years</p>
+							<p class="readouts"><span id="period2"></span> years</p>
 							</div>
-  							<input type="range" min="1" max="5" value="3" class="slider2" id="myPeriodRange" onchange="computeLoan()">
+  							<input type="range" min="1" max="5" value="3" class="slider4" id="myPeriodRange2" onchange="computeLoan2()">
 							</div>
 
-							<div class="flex-container">
+							<div class="flex-container second">
 								<p>Interest rate</p>
-								<p class="readouts">2.0%</p>
+								<p class="readouts">4.0%</p>
 							</div>
 
-							<div class="flex-container">
+							<div class="flex-container second">
 								<p>Monthly payment</p>
-								<p id ="payment"></p>
+								<p id ="payment2"></p>
 							</div>
 						</div>
 
@@ -193,27 +193,27 @@
 							<div class="slidecontainer">
 							<div class="flex-container">
 							<p>Amount</p>
-							<p class="readouts">$ <span id="loans"></span></p>
+							<p class="readouts">$ <span id="loans3"></span></p>
 							</div>
-  							<input type="range" min="1" max="10000" value="1000" class="slider" id="myLoanRange" onchange="computeLoan()">
+  							<input type="range" min="1" max="10000" value="1000" class="slider5" id="myLoanRange3" onchange="computeLoan3()">
 							</div>
 
 							<div class="slidecontainer">
 							<div class="flex-container">
 							<p>Period</p>
-							<p class="readouts"><span id="period"></span> years</p>
+							<p class="readouts"><span id="period3"></span> years</p>
 							</div>
-  							<input type="range" min="1" max="5" value="4" class="slider2" id="myPeriodRange" onchange="computeLoan()">
+  							<input type="range" min="1" max="5" value="4" class="slider6" id="myPeriodRange3" onchange="computeLoan3()">
 							</div>
 
 							<div class="flex-container">
 								<p>Interest rate</p>
-								<p class="readouts">2.0%</p>
+								<p class="readouts">12.0%</p>
 							</div>
 
 							<div class="flex-container">
 								<p>Monthly payment</p>
-								<p id ="payment"></p>
+								<p id ="payment3"></p>
 							</div>
 						</div>
 					</div>
@@ -222,7 +222,7 @@
 
 		<section class="back">
 			<div class="container flex-container apply">
-				<h2>Ready to get started?</h2>
+				<h2 id="scrollToApply2">Ready to get started?</h2>
 				<button id="modalBtn" class="button submit">Submit your application</button>
 				<div id="simpleModal" class="modal">
 					<div class="modal-content">
@@ -240,19 +240,19 @@
 					<p>234 W 42nd St, New York, NY 10036</p>
 					<p>United States</p>
 					<ul class="flex-container">
-                  		<li><a href="#!"><i class="fab fa-linkedin"></i></a></li>
-                  		<li><a href="#!"><i class="fab fa-facebook-square"></i></a></li>
-                  		<li><a href="#!"><i class="fab fa-twitter"></i></a></li>
+                  		<li><a href="https://www.linkedin.com/in/larisabreskaja/"><i class="fab fa-linkedin"></i></a></li>
+                  		<li><a href="https://facebook.com/"><i class="fab fa-facebook-square"></i></a></li>
+                  		<li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a></li>
                 	</ul>
-					<a href="#"> <img src="assets/images/capital.svg" alt="Weinberg footer logo"></a>
-					<p>Copyright © 2020 Weinberg Capital, Inc.</p>
+					<a href="index.php"> <img src="assets/images/capital2.png" alt="Weinberg footer logo"></a>
+					<p id="copyright">Copyright © 2020 Weinberg Capital, Inc.</p>
 				</div>
-					<div class="map">
+				<div class="map">
 					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.254314528276!2d-73.9910278495863!3d40.75643094273268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25854a9d7a5cd%3A0xf7a2e7477b505499!2s234%20W%2042nd%20St%2C%20New%20York%2C%20NY%2010036%2C%20USA!5e0!3m2!1sen!2slt!4v1574255366619!5m2!1sen!2slt"></iframe>
 				</div>
 			</div>
 		</footer>
 	
-	<script type="text/javascript" src="assets/scripts/custom.js"></script>
+		<script type="text/javascript" src="assets/scripts/custom.js"></script>
 	</body>
 </html>
